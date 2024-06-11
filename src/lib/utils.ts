e431f15c946f667fc5281d26cf52a775c4a4a43b
zrjohnson3 <zrjohnson3@gmail.com>
2024-06-11 10:19:37 -0400

    Created helper functions for formatting money and date (formatMoney and relativeDate methods)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { formatDistanceToNowStrict } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Available by default in JavaScript for formatting money by greating this function
+export function formatMoney(amount: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount)
+}
+
+export function formatDate(date: string) {
+  return new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+  }).format(new Date(date))
+}
+
+export function relativeDate(from: Date) {
+  return formatDistanceToNowStrict(from, { addSuffix: true })
 }
