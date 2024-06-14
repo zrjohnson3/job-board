5d125be4b29adfc169aa3c68e2fd6b25b62524b4
zrjohnson3 <zrjohnson3@gmail.com>
2024-06-14 10:15:56 -0400

    Created Footer and  Adjusted layout / metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,14 +24,14 @@ export default async function Home({ searchParams: { q, type, location, remote }
   return (
     <main className="flex min-h-screen flex-col justify-between max-w-5xl m-auto px-3 my-10 space-y-3">
       <div className="text-center tracking-tight">
-        <h1 className="font-extrabold text-xl md:text-3xl lg:text-5xl">Zen Jobs</h1>
+        <h1 className="font-extrabold text-xl md:text-3xl lg:text-5xl">Developer Jobs</h1>
         <h3>Find your dream job!</h3>
       </div>
 
       <section className="flex flex-col-reverse flex-1 md:flex-row">
         <JobResults filterValues={filterValues} />
         <JobFilterSidebar defaultValues={filterValues} />
-        
+
       </section>
     </main>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import { Link as LinkIcon } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='border-t'>
+            <div className='mx-auto max-w-6xl space-y-5 px-3 py-5'>
+                <div className='flex flex-wrap gap-5 items-center justify-evenly'>
+
+                    <Link href='/about' className='hover:underline'>
+                        About Us
+                    </Link>
+                    <Link href='/mission' className='hover:underline'>
+                        Mission
+                    </Link>
+                    <Link href='/contact' className='hover:underline'>
+                        Contact
+                    </Link>
+                </div>
+            </div>
+            <div className='text-center text-sm text-muted-foreground'>
+                {new Date().getFullYear()} Zen Jobs, Inc. All rights reserved.
+            </div>
+        </footer>
+    )
+}
