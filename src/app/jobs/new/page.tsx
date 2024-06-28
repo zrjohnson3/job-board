47ca2452a9616e4d9e5ecfcc16f3138b5010e6dd
zrjohnson3 <zrjohnson3@gmail.com>
2024-06-28 12:37:55 -0400

    New Job Page:  form to post new jobs/positions

---
+++ src/app/jobs/new/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NewJobForm from './NewJobForm';
+
+export const metadata = {
+    title: "Post a new job"
+}
+
+export default function NewJobPage() {
+    return (
+        <div>
+            <NewJobForm />
+        </div>
+
+    )
+}
