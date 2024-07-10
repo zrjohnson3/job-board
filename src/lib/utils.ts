2602ad7bb890b83ab98f31faa1ab575814b511ee
Zachary Johnson <zrjohnson3@gmail.com>
2024-07-09 23:36:22 -0400

    Created toSlug method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,10 @@ export function formatDate(date: string) {
 export function relativeDate(from: Date) {
   return formatDistanceToNowStrict(from, { addSuffix: true })
 }
+
+export function toSlug(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
+}
