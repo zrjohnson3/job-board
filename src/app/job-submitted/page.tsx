7ff2f24fb3d34217efd8361892e0ab43dff61be2
Zachary Johnson <zrjohnson3@gmail.com>
2024-12-26 18:27:30 -0300

    Basic "job-submitted" page / pending approval

---
+++ src/app/job-submitted/page.tsx
@@ -0,0 +1,11 @@
+import H1 from '@/components/ui/h1'
+import React from 'react'
+
+export default function page() {
+    return (
+        <main>
+            <H1>Job Submitted!</H1>
+            <p>Your job posting has been submitted and is pending approval.</p>
+        </main>
+    )
+}
